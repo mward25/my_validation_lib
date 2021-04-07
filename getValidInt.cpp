5c8ec3adf043db07ac8c3d084d0a4752516e4c49
miles <milesward2004@gmail.com>
2021-04-06 18:01:22 -0700

    added integer validation programs

---
+++ getValidInt.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include "getValidInt.hpp"
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+| This is a simple c++ function written by Miles Ward to get a valid integer from the User.    |
+| The function takes 4 arguments, 1, the message the user is to be prompted with, 2, the       |
+| message the user will receive if they did not enter a valid number, 3, the minimum value the |
+| User can enter, and 4, the maximum value the User can enter.                                 |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+
+// Constants
+const int BIGNUM = INT_MAX;
+const char ENDLINE = '\n';
+
+int getValidInt(string promptMessage, string failMessage, int minVal, int maxVal) {
+	// Output promptMessage to the console
+	cout << promptMessage << endl;
+
+	// Variables
+	int inputInt = minVal -1;
+
+	do 
+	{
+			// Get input
+			cin >> inputInt;
+
+			// If there is an error clear error message and flush the buffer
+			if (cin.fail()) 
+			{
+				cin.clear();
+				cin.ignore(BIGNUM, ENDLINE);
+
+				// inputInt is set to a value that will ensure the loop will continue
+				inputInt = minVal -1;
+
+				// the User was wrong, so we tell them
+				cout << failMessage << endl;
+			}
+			else if (inputInt < minVal || inputInt > maxVal) 
+			{
+				// the User was wrong, so we tell them
+				cout << failMessage;
+			}
+		} while (inputInt < minVal || inputInt > maxVal);
+	// return the valid integer
+	return inputInt;
+}
---
+++ getValidInt.hpp
@@ -0,0 +1,5 @@
+#ifndef getValidInt_HPP
+#define getValidInt_HPP
+#include <string>
+int getValidInt(std::string promptMessage, std::string failMessage, int minVal, int maxVal);
+#endif
