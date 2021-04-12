cc9a6337f18a4c2da310068853c6fa4035c983eb
miles <milesward2004@gmail.com>
2021-04-12 13:56:30 -0700

    added getYorN function

---
+++ build/CMakeFiles/myValidationLib.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/miles/cpp_stuff/my_validation_lib/getValidInt.cpp" "/home/miles/cpp_stuff/my_validation_lib/build/CMakeFiles/myValidationLib.dir/getValidInt.cpp.o"
+  "/home/miles/cpp_stuff/my_validation_lib/getYorN.cpp" "/home/miles/cpp_stuff/my_validation_lib/build/CMakeFiles/myValidationLib.dir/getYorN.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ getYorN.hpp
@@ -0,0 +1,10 @@
+#ifndef getYorN_HPP
+#define getYorN_HPP
+
+// constants
+const char YES_CHAR = 'y';
+const char NO_CHAR = 'n';
+
+// functions
+char getYorN(std::string promptMessage, std::string failMessage);
+#endif
