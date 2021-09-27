5e9e02d9ea8db873902b6607bbc18e9174cecdb9
miles <milesward2004@gmail.com>
2021-09-27 14:26:31 -0700

    Added .gitignore, added comment in cmake file to enable compiling in debug mode, made driver use more constants, and made driver output better prompt messages.

--- driver/driver.cpp
+++ driver/driver.cpp
@@ -8,6 +8,10 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::to_string;
+const int MINIMUM_VALUE = 0;
+const int MAXIMUM_VALUE = 100;
+const string INPUT_IS_NOT_VALID = "input is not valid\n";
 int main()
 {
 	const int TEST_CHAR_ARRAY_SIZE = 7; //               1    2    3    4    5    6    7
@@ -24,45 +28,45 @@ int main()
 	string testString;
 	#ifdef USE_OVERLOAD_FOR_VALID
 		cout << "getting valid int" << endl;
-		testInt = getValidNumber("please enter a valid integer", "Input not valid", static_cast<int>(0), static_cast<int>(100));
+		testInt = getValidNumber("please enter a valid integer between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, static_cast<int>(MINIMUM_VALUE), static_cast<int>(MAXIMUM_VALUE));
 		cout << "you entered " << testInt << endl;
 
 		cout << "getting valid double" << endl;
-		testDouble = getValidNumber("please enter a valid double", "Input not valid", static_cast<double>(0), static_cast<double>(100));
+		testDouble = getValidNumber("please enter a valid double between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, static_cast<double>(MINIMUM_VALUE), static_cast<double>(MAXIMUM_VALUE));
 		cout << "you entered " << testDouble << endl;
 
 		cout << "getting valid float" << endl;
-		testFloat = getValidNumber("please enter a valid float", "Input not valid", static_cast<float>(0), static_cast<float>(100));
+		testFloat = getValidNumber("please enter a valid float between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, static_cast<float>(MINIMUM_VALUE), static_cast<float>(MAXIMUM_VALUE));
 		cout << "you entered " << testFloat << endl;
 	#endif
 
 	#ifdef USE_TEMPLATE_FOR_VALID
-		cout << "getting valid int" << endl;
-		testInt = getValidNumber<int>("please enter a valid integer", "Input not valid", 0, 100);
+		cout << "getting valid int using a template function" << endl;
+		testInt = getValidNumber<int>("please enter a valid integer between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, MINIMUM_VALUE, MAXIMUM_VALUE);
 		cout << "you entered " << testInt << endl;
 
-		cout << "getting valid double" << endl;
-		testDouble = getValidNumber<double>("please enter a valid double", "Input not valid", 0, 100);
+		cout << "getting valid double using a template function" << endl;
+		testDouble = getValidNumber<double>("please enter a valid double between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, MINIMUM_VALUE, MAXIMUM_VALUE);
 		cout << "you entered " << testDouble << endl;
 
-		cout << "getting valid float" << endl;
-		testFloat = getValidNumber<float>("please enter a valid float", "Input not valid", 0, 100);
+		cout << "getting valid float using a template function" << endl;
+		testFloat = getValidNumber<float>("please enter a valid float between " + to_string(MINIMUM_VALUE) + " and " + to_string(MAXIMUM_VALUE), INPUT_IS_NOT_VALID, MINIMUM_VALUE, MAXIMUM_VALUE);
 		cout << "you entered " << testFloat << endl;
 	#endif
 
 	cout << "getting y or n" << endl;
-	testChar = getYOrN("please enter y or n", "Input not valid");
+	testChar = getYOrN("please enter y or n", INPUT_IS_NOT_VALID);
 	cout << "you entered" << testChar << endl;
 
 	cout << "getting valid char" << endl;
-	testChar = getValidOption("please enter a y 7 3 5 Y S, this is case sensitive", "input not valid", TEST_CHAR_ARRAY, TEST_CHAR_ARRAY_SIZE, true);
+	testChar = getValidOption("please enter a y 7 3 5 Y S, this is case sensitive", INPUT_IS_NOT_VALID, TEST_CHAR_ARRAY, TEST_CHAR_ARRAY_SIZE, true);
 
 	cout << "getting valid string" << endl;
-	testString = getValidOption("please enter hello jhon Potato Apple Donut appleNumber78 7 a 78899 208epicTest", "Input not valid", TEST_STRING_ARRAY, TEST_STRING_ARRAY_SIZE, false);
+	testString = getValidOption("please enter hello jhon Potato Apple Donut appleNumber78 7 a 78899 208epicTest", INPUT_IS_NOT_VALID, TEST_STRING_ARRAY, TEST_STRING_ARRAY_SIZE, false);
 	cout << "you entered " << testString << endl;
 
 	cout << "getting valid string case sensitive" << endl;
-	testString = getValidOption("please enter hello jhon Potato Apple Donut appleNumber78 7 a 78899 208epicTest", "Input not valid", TEST_STRING_ARRAY, TEST_STRING_ARRAY_SIZE, true);
+	testString = getValidOption("please enter hello jhon Potato Apple Donut appleNumber78 7 a 78899 208epicTest", INPUT_IS_NOT_VALID, TEST_STRING_ARRAY, TEST_STRING_ARRAY_SIZE, true);
 	cout << "you entered " << testString << endl;
 
 }
